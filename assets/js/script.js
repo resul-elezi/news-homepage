fad85f03ddae88de618e181b02f4b96047bce002
Resul Elezi <resul_elezi@protonmail.com>
2024-01-24 19:21:17 +0100

    #

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@
 
 let navBar = document.getElementById("nav-bar");
 let menuIcon = document.getElementById("menu");
+let body = document.getElementById("body");
 
 menuIcon.onclick = function() {
     navBar.classList.toggle("open-menu");
     menuIcon.classList.toggle("open-menu");
+    body.classList.toggle("open-menu");
 
     if(navBar.classList.contains("open-menu")) {
         menuIcon.src = "assets/images/icon-menu-close.svg";
