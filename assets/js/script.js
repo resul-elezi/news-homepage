4e9926a02a15ffa9d0c7c5f91c58dd4064e4fa78
Resul Elezi <resul_elezi@protonmail.com>
2024-05-08 13:51:13 +0200

    #

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,23 @@
 "use strict";
 
-let navBar = document.getElementById("nav-bar");
-let menuIcon = document.getElementById("h-menu");
-let header = document.getElementById("header");
-let overlay = document.getElementById("overlay");
+// let navBar = document.getElementById("nav-bar");
+// let menuIcon = document.getElementById("h-menu");
+// let header = document.getElementById("header");
+// let overlay = document.getElementById("overlay");
 
-menuIcon.onclick = function() {
-    navBar.classList.toggle("open-menu");
-    menuIcon.classList.toggle("open-menu");
-    header.classList.toggle("open-menu");
-    overlay.classList.toggle("open-menu");
+// menuIcon.onclick = function() {
+//     navBar.classList.toggle("open-menu");
+//     menuIcon.classList.toggle("open-menu");
+//     header.classList.toggle("open-menu");
+//     overlay.classList.toggle("open-menu");
 
-    if(navBar.classList.contains("open-menu")) {
-        menuIcon.src = "assets/images/icon-menu-close.svg";
-    }
-    else{
-        menuIcon.src = "assets/images/icon-menu.svg";
-    }
-}
+//     if(navBar.classList.contains("open-menu")) {
+//         menuIcon.src = "assets/images/icon-menu-close.svg";
+//     }
+//     else{
+//         menuIcon.src = "assets/images/icon-menu.svg";
+//     }
+// }
+
+const NAV_BAR = document.querySelector(".nav-bar");
+const NAV_TOGGLE = document.querySelector(".mobile-nav-toggle");
