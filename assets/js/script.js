5405ddf43f5791ec4bcc7f6aaea92a9b85a61eeb
Resul Elezi <resul_elezi@protonmail.com>
2024-05-10 15:58:15 +0200

    #

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,5 @@
 "use strict";
 
-// let navBar = document.getElementById("nav-bar");
-// let menuIcon = document.getElementById("h-menu");
-// let header = document.getElementById("header");
-// let overlay = document.getElementById("overlay");
-
-// menuIcon.onclick = function() {
-//     navBar.classList.toggle("open-menu");
-//     menuIcon.classList.toggle("open-menu");
-//     header.classList.toggle("open-menu");
-//     overlay.classList.toggle("open-menu");
-
-//     if(navBar.classList.contains("open-menu")) {
-//         menuIcon.src = "assets/images/icon-menu-close.svg";
-//     }
-//     else{
-//         menuIcon.src = "assets/images/icon-menu.svg";
-//     }
-// }
-
 const NAV_BAR = document.querySelector(".nav-bar");
 const NAV_TOGGLE = document.querySelector(".mobile-nav-toggle");
 const OVERLAY = document.querySelector(".overlay");
